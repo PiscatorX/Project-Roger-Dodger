44b8e60f44358d022e569894c2ac077935fa132e
Andrew Ndhlovu <drewxdvst@outlook.com>
2020-09-18 00:33:47 +0200

    needs more testing but almost done

--- Python-ML/random_forest.py
+++ Python-ML/random_forest.py
@@ -13,8 +13,13 @@
 #     name: python3
 # ---
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeRegressor
+from matplotlib import pyplot
+from sklearn import metrics
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 np.random.seed(0)
 
@@ -31,10 +36,10 @@ for var in ["Adsorbent","adsorbate","solvent","Batch_Dynamic"]:
 zeolite_df['References'] = zeolite_df['References'].astype('string')
 
 #https://towardsdatascience.com/the-dummys-guide-to-creating-dummy-variables-f21faddb1d40
-zeolite_df.dtypes
+zeolite_df.shape
 #zeolite_df
 
-#convett the categorical variables to intergers also known as one-hot-encoding
+#convert the categorical variables to intergers also known as one-hot-encoding
 #https://towardsdatascience.com/the-dummys-guide-to-creating-dummy-variables-f21faddb1d40
 #This can be automated to identify only those categorical variables, encode them and save them to a list for concat
 encoded_Adsorbent = pd.get_dummies(zeolite_df['Adsorbent'])
@@ -46,7 +51,7 @@ encoded_Batch_Dynamic = pd.get_dummies(zeolite_df['Batch_Dynamic'])
 #remove the categoricol variable columns and any unwanted columns in this case "References"
 #this can also be automated as part of the step abov
 zeolite_dropped = zeolite_df.drop(["Adsorbent","solvent","adsorbate","Batch_Dynamic", "References"],axis= 1)
-#zeolite_dropped
+#zeolite_dropped.dtypes
 
 # +
 #encoded_Adsorbent
@@ -54,9 +59,70 @@ zeolite_dropped = zeolite_df.drop(["Adsorbent","solvent","adsorbate","Batch_Dyna
 #encoded_Batch_Dynamic  
 # -
 
-zeolite_final = pd.concat([zeolite_dropped, encoded_Adsorbent,encoded_Batch_Dynamic], axis=1)
+zeolite_final = pd.concat([zeolite_dropped, encoded_Adsorbent, encoded_Batch_Dynamic],axis=1 )
 
+# +
 #We have our data ready for machine learning
-zeolite_final.shape
+#zeolite_final.dtypes
+
+# +
+#https://stackabuse.com/random-forest-algorithm-with-python-and-scikit-learn/
+#zeolite_final.columns
+# -
+
+#attributes 
+y = zeolite_final.loc[:,"Capacity"].values
+#labels
+X = zeolite_final.drop(["Capacity"], axis = 1).values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+#Standardize features by removing the mean and scaling to unit variasnce
+sc = StandardScaler()
+#https://datascience.stackexchange.com/questions/12321/whats-the-difference-between-fit-and-fit-transform-in-scikit-learn-models#:~:text=%22fit%22%20computes%20the%20mean%20and,both%20at%20the%20same%20time.
+#This should not make much of a difference but its good practice
+#TO DO
+#Compare accuracy with and without scaling
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+regressor = RandomForestRegressor(n_estimators=1000, random_state=0)
+#TO DO
+#increase n_estimators
+#run in parallel
+
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+errors = abs(y_pred - y_test)
+
+mape = 100 * (errors / y_test)
+# Calculate and display accuracy
+accuracy = 100 - np.mean(mape)
+print('Accuracy:', round(accuracy, 2), '%.')
+
+model = DecisionTreeRegressor()
+# fit the model
+model.fit(X, y)
+# get importance
+importance = model.feature_importances_
+# summarize feature importance
+for i,v in enumerate(importance):
+	print('Feature: %0d, Score: %.5f' % (i,v))
+# plot feature importance
+pyplot.bar([x for x in range(len(importance))], importance)
+pyplot.show()
+
+X = zeolite_final.drop(["Capacity"], axis = 1)
+X.iloc[:,19]
+
+X = zeolite_final.drop(["Capacity"], axis = 1)
+feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+feat_importances.nlargest(20).plot(kind='barh')
 
 
