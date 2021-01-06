9a2c51ac4eedd63fcc090615a8ea85558f9ca0af
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-07 01:24:01 +0200

    Filtering duplicates in Adosorbents feature

--- R-ML/prepocx.R
+++ R-ML/prepocx.R
@@ -20,6 +20,22 @@ t(missing)
 write.table(missing, "zeolite.miss", sep="\t", row.names = F, quote = F)
 
 #Adsorbetcounts
-Adsorbent_analysis <-zeolite %>% group_by(Adsorbent) %>% summarise(n=n())
+Adsorbent_analysis <-zeolite %>% group_by(Adsorbent) %>% summarise(count=n())
 
+Adsorbent_analysis %>% filter(count<=2) %>%  arrange(Adsorbent) %>% data.frame()
+#Singletone maybe typos
+#eg Ag-Y vs AgY
+#What about CsY could it be CeY
+
+#Missing hyphens may be an issue
+#Count duplicates when hyphen are removed
+gsub("-", "", Adsorbent_analysis$Adsorbent) %>% as.tibble() %>% group_by(value)  %>% summarise(count=n()) %>% filter(count != 1)
+#These below need to be checked
+# 1 AgY       2
+# 2 CeY       2
+# 3 CuY       2
+# 4 HY        2
+# 5 NaY       2
+# 6 NiCeY     2
+# 7 NiY       2
 
