21a4400aea59c91cbacb232bcc03a54374cd958d
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-06 23:42:05 +0200

    data exploration, check missing data

--- R-ML/prepocx.R
+++ R-ML/prepocx.R
@@ -1,3 +1,24 @@
-zeolite = read.table("zeolitex_final.tsv", sep = "\t", header = T)
-zeolite2 = read.table("zeolitex_final_na.tsv", sep = "\t", header = T)
+library(magrittr)
+library(dplyr)
+library(tidyverse)
 
+
+zeolite = read.table("zeolitex_final.tsv", sep = "\t", header = T)
+
+colnames(zeolitex) %>% data.frame() 
+ 
+zeolite <- zeolite %>% mutate_all(na_if,"") 
+
+N <- nrow(zeolite)
+
+zeolite %>% summarise_all(funs(sum(is.na(.)))) %>% data.frame() 
+
+
+
+zeolite %>% summarise(n = n())
+
+
+#zeolite %>% select(Adsorbent) 
+
+#%>% summarise_all(funs(sum(is.na(.)))) 
+zeolite$solvent == ""
