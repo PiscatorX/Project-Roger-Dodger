5674634c6bd0aa527f16deb159c931290bfd47a3
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-07 18:36:18 +0200

    Surface area exploratory analysis

--- R-ML/prepocx.R
+++ R-ML/prepocx.R
@@ -1,7 +1,8 @@
 library(magrittr)
 library(dplyr)
 library(tidyverse)
-
+library(ggplot2)
+library(ggpubr)
 
 zeolite = read.table("zeolitex_final.tsv", sep = "\t", header = T)
 
@@ -20,16 +21,30 @@ t(missing)
 write.table(missing, "zeolite.miss", sep="\t", row.names = F, quote = F)
 
 #Adsorbetcounts
-Adsorbent_analysis <-zeolite %>% group_by(Adsorbent) %>% summarise(count=n())
+Adsorbent_analysis <-zeolite %>% group_by(Adsorbent) %>% summarise(Count=n()) %>% arrange(desc(Count))
 
-Adsorbent_analysis %>% filter(count<=2) %>%  arrange(Adsorbent) %>% data.frame()
+p <- ggbarplot(Adsorbent_analysis,  x  = "Adsorbent",  y = "Count",
+          fill = "Count",
+          sort.by.groups = FALSE,
+          sort.val = "asc")
+
+ggpar(p, x.text.angle = 45)
+
+Adsorbent_analysis %>% filter(Count<=2) %>%  arrange(Adsorbent) %>% data.frame()
 #Singletone maybe typos
 #eg Ag-Y vs AgY
 #What about CsY could it be CeY
 
 #Missing hyphens may be an issue
 #Count duplicates when hyphen are removed
-gsub("-", "", Adsorbent_analysis$Adsorbent) %>% as.tibble() %>% group_by(value)  %>% summarise(count=n()) %>% filter(count != 1)
+
+gsub("-", "", Adsorbent_analysis$Adsorbent) %>% 
+              as.tibble() %>% 
+              group_by(value) %>% 
+              summarise(count=n()) %>% 
+              filter(count != 1) %>%
+              data.frame()
+
 #These below need to be checked
 # 1 AgY       2
 # 2 CeY       2
@@ -38,4 +53,36 @@ gsub("-", "", Adsorbent_analysis$Adsorbent) %>% as.tibble() %>% group_by(value) 
 # 5 NaY       2
 # 6 NiCeY     2
 # 7 NiY       2
+
+#Surface area
+ggdotchart(data = zeolite %>% filter(!is.na(SA)),
+           x = "Adsorbent",
+           y = "SA",
+           color = "Adsorbent",
+           sorting = "descending",
+           ggtheme = theme_pubr()) + 
+           theme(legend.position = "none")
+
+
+SA <- zeolite %>% select(SA) %>% filter(!is.na(SA))
+
+
+ggplot(data=SA,
+            aes(x = SA)) +  
+            geom_density(aes(y = ..count..)) +
+            geom_vline(aes(xintercept = mean(SA)), 
+            linetype = "dashed", size = 0.6) + 
+            ylab("Density") +
+            theme_pubr()
+           
+  
+ggviolin(data = zeolite %>% select(SA) %>% filter(!is.na(SA)),
+         y = "SA",
+         palette = c("#00AFBB", "#E7B800", "#FC4E07"),
+         add = "boxplot")  
+
+iqr = zeolite$SA  %>% select(SA) %>% filter(!is.na(SA))
+IQR(x = )
+
+
 
