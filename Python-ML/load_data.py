06ef369888c4f0a8d00ffe286544fe9e2db8d8e9
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-15 21:38:36 +0200

    group means capacity

--- Python-ML/load_data.py
+++ Python-ML/load_data.py
@@ -7,86 +7,92 @@ import sys
 
 
 
-def  get_zeo(filename,outfile):
+class GetZeoliteTsv(object):
 
-    #importing into pandas dataframe
-    zeolite_df = pd.read_csv(filename, skipinitialspace=True, delimiter = "\t")
+    def __init__(self,
+                 zeolite_filename,
+                 zeolite_outfile,
+                 categorical_cols = ["Adsorbent","adsorbate","solvent","Batch_Dynamic"]):
 
-    
-    zeolite_df["Adsorbent"]
+        """ Get zeolite tsv  file exported from excel """
+        
 
-    
-    for var in ["SA","Vmicro","Vmeso","pore size","Si_Al","Ag","Ce","Cu","C_start","C_end", "adsorbent"]:
-            #zeolite_df[var] =  zeolite_df[var].fillna(0)
-            pprint.pprint(zeolite_df[var])
-            zeolite_df[var] =  zeolite_df[var].astype('float64')
+        self.zeolite_df = pd.read_csv(zeolite_filename,
+                                      delimiter = "\t",
+                                      skipinitialspace=True)
+        self.zeolite_outfile = zeolite_outfile
+        self.categorical_cols = categorical_cols
+        self.float_cols = list(set(self.zeolite_df.columns) - set(self.categorical_cols))
+
+        
+    def parse_zeo(self):
+
+        """ Parse the zeolite data frame, assign dytpes and impute missing values"""
+        
+        for float_var in self.float_cols:
             
+            self.zeolite_df[float_var] =  self.zeolite_df[float_var].astype('float64')
 
-    for var in ["Adsorbent","adsorbate","solvent","Batch_Dynamic"]:
-        zeolite_df[var] =  zeolite_df[var].astype('category')
+        for cat_var in self.categorical_cols:
+            self.zeolite_df[cat_var] =  self.zeolite_df[cat_var].str.rstrip().astype('category')
 
-    zeolite_df['References'] = zeolite_df['References'].astype('string')
+        #https://towardsdatascience.com/the-dummys-guide-to-creating-dummy-variables-f21faddb1d40
+        #convert the categorical variables to intergers also known as one-hot-encoding
+        #https://towardsdatascience.com/the-dummys-guide-to-creating-dummy-variables-f21faddb1d40
+        #This can be automated to identify only those categorical variables, encode them and save them to a list for concat
+        encoded_Adsorbent = pd.get_dummies(self.zeolite_df['Adsorbent'])
+        encoded_solvent = pd.get_dummies(self.zeolite_df['solvent'])
 
-    #https://towardsdatascience.com/the-dummys-guide-to-creating-dummy-variables-f21faddb1d40
-    #convert the categorical variables to intergers also known as one-hot-encoding
-    #https://towardsdatascience.com/the-dummys-guide-to-creating-dummy-variables-f21faddb1d40
-    #This can be automated to identify only those categorical variables, encode them and save them to a list for concat
-    encoded_Adsorbent = pd.get_dummies(zeolite_df['Adsorbent'])
-    encoded_solvent = pd.get_dummies(zeolite_df['solvent'])
-    #No need to encode this, all values appear to be same?
-    #encoded_adsorbate = pd.get_dummies(zeolite_df['adsorbate'])
-    encoded_Batch_Dynamic = pd.get_dummies(zeolite_df['Batch_Dynamic'])
+        #No need to encode this, all values appear to be same?
+        encoded_adsorbate = pd.get_dummies(self.zeolite_df['adsorbate'])
+        encoded_Batch_Dynamic = pd.get_dummies(self.zeolite_df['Batch_Dynamic'])
 
-    encoded_Adsorbent
+        #remove the categorical variable columns and any unwanted columns in this case "References"
+        #this can also be automated as part of the step above
+        zeolite_dropped =self.zeolite_df.drop(["Adsorbent","solvent","adsorbate","Batch_Dynamic"],axis= 1)  
+        zeolite_final = pd.concat([zeolite_dropped,
+                                   encoded_solvent,
+                                   encoded_Adsorbent,
+                                   encoded_Batch_Dynamic],
+                                   axis=1 )
 
-    #remove the categorical variable columns and any unwanted columns in this case "References"
-    #this can also be automated as part of the step abov
-    zeolite_dropped = zeolite_df.drop(["Adsorbent","solvent","adsorbate","Batch_Dynamic", "References"],axis= 1)
-    #zeolite_dropped.dtypes
-
-    #encoded_Adsorbent
-    encoded_solvent 
-    #encoded_Batch_Dynamic  
-
-    zeolite_final = pd.concat([zeolite_dropped, encoded_solvent, encoded_Adsorbent, encoded_Batch_Dynamic],axis=1 )
-
-    SA_imputation(zeolite_df)
-
-    sys.exit(1)
-    
-    zeolite_dropped
-
-    #We have our data ready for machine learning
-    zeolite_final.dtypes
-    #saving dataframe to file for optimisation
-    zeolite_final.to_csv(outfile, sep='\t', index = False)
-
-    return zeolite_final
+#     SA_imputation(zeolite_df)
+#     sys.exit(1)
+#     zeolite_dropped
+#     #We have our data ready for machine learning
+#     zeolite_final.dtypes
+#     #saving dataframe to file for optimisation
+#     
+#     return zeolite_final
 
 
-def SA_imputation(zeolite_df):
 
-    Adsorbent_counts = zeolite_df['Adsorbent'].value_counts()
+    def GroupMeanImputation(self, grp_var_col, impute_val_col):
 
-     
-    SA_nan = zeolite_df[zeolite_df.loc[:,'SA'].isna()]
+        """Impute values in one column using group variable columns"""
 
-    print(zeolite_df.loc[:,['Adsorbent','SA']])
+        #use group means to fill in missing values
+        #Singletons remain NaN
+        self.zeolite_df[impute_val_col] = \
+            self.zeolite_df[impute_val_col].fillna(self.zeolite_df.groupby(grp_var_col)
+                                                                                             [impute_val_col].transform('mean'))
 
-    print(zeolite_df.groupby('Adsorbent',as_index=False)['SA'].mean())
-    
-    
-    for idx, row  in SA_nan.loc[:,['Adsorbent','SA']].iterrows():
-        Ads, SA = row
-        #print(Ads)
-    
+        
+
+    def save_zeo(self):
+
+        """Save zeolite to tsv for training"""
+
+        self.zeolite_final.to_csv(outfile, sep='\t', index = False)  
+
+
     
 if  __name__  == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-z','--zeolite_file',help ="sequence file", type=argparse.FileType('r'), default = "/home/drewx/Documents/Project-Roger-Dodger/data/zeolites_review_TP.csv", required=False)
+    parser.add_argument('zeolite_file', help ="sequence file", type=argparse.FileType('r'))
     parser.add_argument('-o','--outfile', default = "zeolitex_final.tsv", type=argparse.FileType('w'), required = False)
     args = parser.parse_args()
-    get_zeo(args.zeolite_file, args.outfile)
-
-
+    getZeo = GetZeoliteTsv(args.zeolite_file, args.outfile)
+    getZeo.parse_zeo() 
+    getZeo.GroupMeanImputation('Absorbent','SA')
 
