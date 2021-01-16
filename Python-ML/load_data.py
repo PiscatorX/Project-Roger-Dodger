787ec868fe7bf504099ded7999f3232af38ceb22
U-HP-LAPTOP\DrewX <drewxdvst@outlook.com>
2021-01-16 18:33:34 +0200

    finalise imputation

--- Python-ML/load_data.py
+++ Python-ML/load_data.py
@@ -20,41 +20,74 @@ class GetZeoliteTsv(object):
         self.zeolite_df = pd.read_csv(zeolite_filename,
                                       delimiter = "\t",
                                       skipinitialspace=True)
-        self.zeolite_outfile = zeolite_outfile
-        self.categorical_cols = categorical_cols
-        self.float_cols = list(set(self.zeolite_df.columns) - set(self.categorical_cols))
 
+        self.df_cols = self.zeolite_df.columns
+        self.zeolite_outfile = zeolite_outfile
+        self.col_dtype = { col: 'category' for col in categorical_cols }    
+        self.col_dtype.update({col: 'category' for col in self.df_cols if col not in categorical_cols })
+        
+    
         
     def parse_zeo(self):
 
         """ Parse the zeolite data frame, assign dytpes and impute missing values"""
-        
-        for float_var in self.float_cols:
-            
-            self.zeolite_df[float_var] =  self.zeolite_df[float_var].astype('float64')
 
-        for cat_var in self.categorical_cols:
-            self.zeolite_df[cat_var] =  self.zeolite_df[cat_var].str.rstrip().astype('category')
+        for col in self.df_cols:
+            self.zeolite_df[col] = self.zeolite_df[col].astype(self.col_dtype[col])
+
+            
+
+    def encode_categorical(self, *categories):
+
+
+        """ One hot encoding for categorical variables """
 
         #https://towardsdatascience.com/the-dummys-guide-to-creating-dummy-variables-f21faddb1d40
         #convert the categorical variables to intergers also known as one-hot-encoding
         #https://towardsdatascience.com/the-dummys-guide-to-creating-dummy-variables-f21faddb1d40
-        #This can be automated to identify only those categorical variables, encode them and save them to a list for concat
-        encoded_Adsorbent = pd.get_dummies(self.zeolite_df['Adsorbent'])
-        encoded_solvent = pd.get_dummies(self.zeolite_df['solvent'])
+        
+        encoded_categories = [ pd.get_dummies(self.zeolite_df[category]) for category in categories ]
+        
+        zeolite_dropped = self.zeolite_df.drop(list(categories), axis= 1)
+        
+        self.zeolite_df = pd.concat( [zeolite_dropped] + encoded_categories, axis=1)
+ 
 
-        #No need to encode this, all values appear to be same?
-        encoded_adsorbate = pd.get_dummies(self.zeolite_df['adsorbate'])
-        encoded_Batch_Dynamic = pd.get_dummies(self.zeolite_df['Batch_Dynamic'])
+        
+    def GroupMeanImputation(self, grp_var_col, impute_val_col):
 
-        #remove the categorical variable columns and any unwanted columns in this case "References"
-        #this can also be automated as part of the step above
-        zeolite_dropped =self.zeolite_df.drop(["Adsorbent","solvent","adsorbate","Batch_Dynamic"],axis= 1)  
-        zeolite_final = pd.concat([zeolite_dropped,
-                                   encoded_solvent,
-                                   encoded_Adsorbent,
-                                   encoded_Batch_Dynamic],
-                                   axis=1 )
+        """Impute values in one column using group variable columns"""
+
+        #use group means to fill in missing values
+        #Singletons remain NaN
+
+        #print(self.zeolite_df.groupby(grp_var_col)[impute_val_col].transform('mean'))
+        
+        #self.zeolite_df[impute_val_col] = \
+            #self.zeolite_df[impute_val_col].fillna(self.zeolite_df.groupby(grp_var_col)[impute_val_col].transform('mean'))        
+
+        
+        
+    def save_zeo(self):
+
+        """Save zeolite to tsv for training"""
+
+        self.zeolite_df.to_csv(self.zeolite_outfile, sep='\t', index = False)  
+
+
+    
+if  __name__  == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('zeolite_file', help ="sequence file", type=argparse.FileType('r'))
+    parser.add_argument('-o','--outfile', default = "ZeoX_Final.tsv", required = False)
+    args = parser.parse_args()
+    getZeo = GetZeoliteTsv(args.zeolite_file, args.outfile)
+    getZeo.parse_zeo() 
+    getZeo.GroupMeanImputation('Absorbent','SA')
+    getZeo.encode_categorical("Adsorbent","solvent","adsorbate","Batch_Dynamic")
+    getZeo.save_zeo()
+
+    
 
 #     SA_imputation(zeolite_df)
 #     sys.exit(1)
@@ -64,35 +97,3 @@ class GetZeoliteTsv(object):
 #     #saving dataframe to file for optimisation
 #     
 #     return zeolite_final
-
-
-
-    def GroupMeanImputation(self, grp_var_col, impute_val_col):
-
-        """Impute values in one column using group variable columns"""
-
-        #use group means to fill in missing values
-        #Singletons remain NaN
-        self.zeolite_df[impute_val_col] = \
-            self.zeolite_df[impute_val_col].fillna(self.zeolite_df.groupby(grp_var_col)
-                                                                                             [impute_val_col].transform('mean'))
-
-        
-
-    def save_zeo(self):
-
-        """Save zeolite to tsv for training"""
-
-        self.zeolite_final.to_csv(outfile, sep='\t', index = False)  
-
-
-    
-if  __name__  == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('zeolite_file', help ="sequence file", type=argparse.FileType('r'))
-    parser.add_argument('-o','--outfile', default = "zeolitex_final.tsv", type=argparse.FileType('w'), required = False)
-    args = parser.parse_args()
-    getZeo = GetZeoliteTsv(args.zeolite_file, args.outfile)
-    getZeo.parse_zeo() 
-    getZeo.GroupMeanImputation('Absorbent','SA')
-
