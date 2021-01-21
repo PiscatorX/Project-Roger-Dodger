2c084123ef4231657a90c04e0355540f54d45d71
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-21 18:46:00 +0200

    generate multiplot

--- Python-ML/SA_imputation.py
+++ Python-ML/SA_imputation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
+# In[1]:
 
 
 from load_data import GetZeoliteTsv
@@ -9,75 +9,90 @@ from matplotlib import pyplot
 import seaborn as sns
 
 
-# In[ ]:
+# In[2]:
 
 
-
+getZeo = GetZeoliteTsv("New_data_imputation.tsv", "Imputed_out.tsv")
 
 
 # In[3]:
 
 
-getZeo = GetZeoliteTsv("New_data_imputation.tsv", "Imputated_Data.tsv")
+#This assigns data types and ensures that we have the correct data tupes categories vs floats etc
+#for more details view the load_data.py file
+zeolite = getZeo.parse_zeo()
 
 
 # In[4]:
 
 
-zeolite = getZeo.parse_zeo()
+#check the file
+zeolite.head()
+
+
+# In[5]:
+
+
+#This creates a new column of name 'imputed'
+#Assign 'Yes' to rows where SA is empty
+#These are there rows that will be imputed
+zeolite.loc[zeolite['SA'].isna(),'imputed'] = 'Yes'
 
 
 # In[6]:
 
 
+#Assign 'No' to the rows that have values
+zeolite.loc[zeolite['SA'].notna(),'imputed'] = 'No'
+
+
+# In[7]:
+
+
+#make the datatype of the column category
+zeolite['imputed'] = zeolite['imputed'].astype('category')
+
+
+# In[8]:
+
+
+#Check the file
 zeolite.head()
 
 
 # In[9]:
 
 
-zeolite.loc[zeolite['SA'].isna(),'imputed'] = 'imputed'
+#Fill in missing date by grouping Adsorbents
+#Means are calculated from each group and then nan are replaced
+#Some value remain unimputed
+getZeo.GroupMeanImputation('Adsorbent','SA')
 
 
 # In[10]:
 
 
-zeolite.head()
+getZeo.zeolite_df.head()
 
 
-# In[15]:
+# In[11]:
 
 
-sns.scatterplot(data=zeolite, x="SA", y="Capacity")
+#visualise imputed data
+sns.scatterplot(data = zeolite, x="SA", y="Capacity", hue='imputed', legend=True)
 
 
-# In[16]:
+# In[12]:
 
 
-get_ipython().run_line_magic('pinfo', 'sns.scatterplot')
+#plot SA vs Capacity  grouped by type of Adsorbent
+#Plot data and regression model fits
+sns.lmplot(data = zeolite, x="SA", y="Capacity", hue="imputed", row="Adsorbent")
 
 
-# In[ ]:
+# In[13]:
 
 
-zeolite.head()
-
-
-# In[ ]:
-
-
-getZeo.GroupMeanImputation('Adsorbent','SA')
-
-
-# In[ ]:
-
-
-getZeo.encode_categorical("Adsorbent","solvent","adsorbate","Batch_Dynamic")
-
-
-# In[ ]:
-
-
+#save imputed output file
 getZeo.save_zeo()
-#GetZeoliteTsv(object)
 
