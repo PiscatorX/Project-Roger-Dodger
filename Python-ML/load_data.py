fba8f8b5433e6d2869f96db041c606dc2c8ffa07
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-03-17 17:37:06 +0200

    update  function parameters

--- Python-ML/load_data.py
+++ Python-ML/load_data.py
@@ -21,11 +21,10 @@ class GetZeoliteTsv(object):
                                       skipinitialspace=True)
 
         
-        print(self.zeolite_df.columns)
+        #print(self.zeolite_df.columns)
         
         self.float_cols = self.zeolite_df.select_dtypes(include=['float64']).columns
         self.zeolite_df.dropna(inplace=True, how = 'all')
-        self.zeolite_outfile = zeolite_outfile
         self.df_dtypes = { col: 'category' for col in self.zeolite_df.columns if col not in self.float_cols }
         
     
@@ -34,7 +33,7 @@ class GetZeoliteTsv(object):
 
         """ Parse the zeolite data frame, assign categorical/float types"""
         
-        print(self.df_dtypes)
+        #print(self.df_dtypes)
         for col in self.df_dtypes:
             try:
                 self.zeolite_df[col] = self.zeolite_df[col].astype(self.df_dtypes[col])
@@ -53,7 +52,7 @@ class GetZeoliteTsv(object):
 
 
         
-    def missingness(self):
+    def missingness(self, miss_outfile):
         
         """ Calculate missingness """
 
@@ -65,7 +64,9 @@ class GetZeoliteTsv(object):
 
         df_miss = df_miss.round(2)
 
-        df_miss.to_csv(self.zeolite_outfile+'.miss', sep='\t', index = False)
+        df_miss.to_csv(miss_outfile,  sep='\t', index = False)
+
+        return df_miss
 
 
     def MeanImputation(self, var_col):
@@ -73,7 +74,7 @@ class GetZeoliteTsv(object):
         """Impute values in one column using group variable columns"""
 
         #use group means to fill in missing values
-        print("MeanImputation: {}".format(var_col))
+        #print("MeanImputation: {}".format(var_col))
         self.zeolite_df[var_col] =  self.zeolite_df[var_col].fillna(self.zeolite_df[var_col].mean())
         
 
@@ -85,7 +86,7 @@ class GetZeoliteTsv(object):
 
         #use group means to fill in missing values
         #Singletons remain NaN
-        print("GroupMeanImputation: {}>>{}".format(grp_var_col, impute_val_col))        
+        #print("GroupMeanImputation: {}>>{}".format(grp_var_col, impute_val_col))        
         self.zeolite_df[impute_val_col] = \
             self.zeolite_df[impute_val_col].fillna(self.zeolite_df.groupby(grp_var_col)[impute_val_col].transform('mean'))        
         
@@ -106,11 +107,11 @@ class GetZeoliteTsv(object):
         self.zeolite_df = pd.concat( [zeolite_dropped] + encoded_categories, axis=1)
          
             
-    def save_zeo(self):
+    def save_zeo(self, zeolite_outfile):
 
         """Save zeolite to tsv for training"""
-
-        self.zeolite_df.to_csv(self.zeolite_outfile, sep='\t', index = False)  
+        
+        self.zeolite_df.to_csv(zeolite_outfile, sep='\t', index = False)  
 
 
 if  __name__  == '__main__':
@@ -119,7 +120,7 @@ if  __name__  == '__main__':
     parser.add_argument('zeolite_file', help ="sequence file", type=argparse.FileType('r'))
     parser.add_argument('-o','--outfile', default = "ZeoX_Final_encoded.tsv", required = False)
     args = parser.parse_args()
-    getZeo = GetZeoliteTsv(args.zeolite_file, args.outfile)
+    getZeo = GetZeoliteTsv(args.zeolite_file)
     getZeo.set_dtypes()
     getZeo.missingness()
     getZeo.GroupMeanImputation('Adsorbent','SA')
@@ -142,4 +143,4 @@ if  __name__  == '__main__':
          getZeo.MeanImputation(var)
 
     getZeo.encode_categorical()
-    getZeo.save_zeo()
+    getZeo.save_zeo(args.outfile)
--- Python-ML/random_forest.py
+++ Python-ML/random_forest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[35]:
 
 
 from sklearn.model_selection import train_test_split
@@ -15,10 +15,11 @@ from scipy import stats
 import seaborn as sns
 import pandas as pd
 import numpy as np
-np.random.seed(0)
+import os
+np.random.seed(1)
 
 
-# In[3]:
+# In[2]:
 
 
 #zeolite datafile exported from excel
@@ -27,7 +28,7 @@ zeolite_fname = "/home/drewx/Documents/Project-Roger-Dodger/Python-ML/zeolites d
 zeolite_outfile = "ZeoX_Final_encoded.tsv" 
 
 
-# In[4]:
+# In[3]:
 
 
 #open the raw tsv data file 
@@ -35,14 +36,14 @@ zeolite_outfile = "ZeoX_Final_encoded.tsv"
 zeolite_fileObj = open(zeolite_fname)
 
 
-# In[5]:
+# In[4]:
 
 
 #create an instance to start processing the datafile
 getZeo = GetZeoliteTsv(zeolite_fileObj, zeolite_outfile)
 
 
-# In[6]:
+# In[5]:
 
 
 #Sanity check of datatypes
@@ -51,35 +52,35 @@ getZeo = GetZeoliteTsv(zeolite_fileObj, zeolite_outfile)
 getZeo.set_dtypes()
 
 
-# In[8]:
+# In[6]:
 
 
 #this counts the missing records per column and saves them to provided filename
 getZeo.missingness("ZeoX_Final_encoded.miss")
 
 
-# In[9]:
+# In[7]:
 
 
 #take not of number of columns
 getZeo.zeolite_df.shape
 
 
-# In[10]:
+# In[8]:
 
 
 #Drops empty columns inplace
 getZeo.zeolite_df.dropna(how='all', axis=1, inplace = True)
 
 
-# In[11]:
+# In[9]:
 
 
 #Very that columns have indeed been lost
 getZeo.zeolite_df.shape
 
 
-# In[12]:
+# In[10]:
 
 
 #Imputation: step by step for easy debugging
@@ -88,42 +89,42 @@ getZeo.GroupMeanImputation('Adsorbent','SA')
 getZeo.MeanImputation('SA')
 
 
-# In[13]:
+# In[11]:
 
 
 getZeo.GroupMeanImputation('Adsorbent','Vmicro')
 getZeo.MeanImputation('Vmicro')
 
 
-# In[14]:
+# In[12]:
 
 
 getZeo.GroupMeanImputation('Adsorbent','Vmeso')
 getZeo.MeanImputation('Vmeso')
 
 
+# In[13]:
+
+
+getZeo.GroupMeanImputation('Adsorbent','pore_size')
+getZeo.MeanImputation('pore_size')
+
+
+# In[14]:
+
+
+getZeo.GroupMeanImputation('Adsorbent','pore_size')
+getZeo.MeanImputation('pore_size')
+
+
 # In[15]:
-
-
-getZeo.GroupMeanImputation('Adsorbent','pore_size')
-getZeo.MeanImputation('pore_size')
-
-
-# In[16]:
-
-
-getZeo.GroupMeanImputation('Adsorbent','pore_size')
-getZeo.MeanImputation('pore_size')
-
-
-# In[17]:
 
 
 getZeo.GroupMeanImputation('Adsorbent','Si_Al')
 getZeo.MeanImputation('Si_Al')
 
 
-# In[18]:
+# In[16]:
 
 
 #Mean imputations only for these variables
@@ -132,13 +133,13 @@ for var in ["C_0","oil_adsorbent_ratio","Temp"]:
          getZeo.MeanImputation(var)
 
 
-# In[19]:
+# In[17]:
 
 
 getZeo.zeolite_df.columns
 
 
-# In[20]:
+# In[18]:
 
 
 #Fill missing values for metals with zeros
@@ -146,7 +147,7 @@ for metal in ['Na', 'Ag', 'Ce', 'Cu', 'Ni', 'Zn','Cs']:
          getZeo.zerofill(metal)
 
 
-# In[21]:
+# In[19]:
 
 
 #convert the categorical variables to intergers also known as one-hot-encoding
@@ -154,28 +155,28 @@ for metal in ['Na', 'Ag', 'Ce', 'Cu', 'Ni', 'Zn','Cs']:
 getZeo.encode_categorical()
 
 
-# In[22]:
+# In[20]:
 
 
 #save the new data to a tsv file
 getZeo.save_zeo("ZeoX_Final_encoded.tsv")
 
 
-# In[23]:
+# In[21]:
 
 
 #get our dataframe 
 zeolite_final  = getZeo.zeolite_df
 
 
-# In[24]:
+# In[22]:
 
 
 #check our dataframe
 zeolite_final.shape
 
 
-# In[25]:
+# In[23]:
 
 
 #We extract our data features 
@@ -185,26 +186,26 @@ y = zeolite_final.loc[:,"Capacity"].values
 X = zeolite_final.drop(["Capacity"], axis = 1).values
 
 
-# In[26]:
+# In[24]:
 
 
 #Split our data into training and test dataset 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 
-# In[27]:
+# In[25]:
 
 
 y_train.shape
 
 
-# In[28]:
+# In[26]:
 
 
 y_test.shape
 
 
-# In[29]:
+# In[27]:
 
 
 #Standardize features by removing the mean and scaling to unit variasnce
@@ -217,7 +218,7 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 
-# In[30]:
+# In[28]:
 
 
 regressor = RandomForestRegressor(n_estimators=1000, random_state=0)
@@ -226,47 +227,60 @@ regressor = RandomForestRegressor(n_estimators=1000, random_state=0)
 #run in parallel
 
 
-# In[30]:
+# In[29]:
 
 
 regressor.fit(X_train, y_train)
 
 
-# In[122]:
+# In[30]:
 
 
-data_table = pd.DataFrame.from_dict({'y_pred': y_pred, 'y_test': y_test, 'errors': abs(y_pred - y_test)})
+y_pred = regressor.predict(X_test)
 
 
-# In[64]:
+# In[31]:
+
+
+data_table = pd.DataFrame.from_dict({'y_pred': y_pred, 'y_test': y_test, 'errors': y_pred - y_test, 'abs_errors': abs(y_pred - y_test)})
+
+
+# In[32]:
 
 
 pd.options.display.max_rows = 4000
-
-
-# In[123]:
-
-
 print(data_table)
 
 
-# In[74]:
+# In[36]:
 
 
-slope, intercept, r_value, p_value, std_err = stats.linregress(data_table)
+np.mean(data_table['errors']**2)
 
 
-# In[82]:
+# In[ ]:
+
+
+#data_table.drop(43, inplace = True)
+
+
+# In[37]:
+
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(data_table['y_pred'], data_table['y_test'])
+
+
+# In[38]:
 
 
 print("Correlation coefficient (R): {:.4f} ".format(r_value))
 print("p-value : {}".format(p_value))
 print("Intercept: {:.4f}".format(intercept))
 print("Slope: {:.4f}".format(slope))
-print("std_eff: {:.4f}".format(std_err))
+print("std_error: {:.4f}".format(std_err))
 
 
-# In[115]:
+# In[39]:
 
 
 ax = sns.regplot(y="y_pred",
@@ -278,31 +292,13 @@ ax = sns.regplot(y="y_pred",
 
 plt.ylabel('Predicted adsorptive capacity (mgS/g)')
 plt.xlabel('Experimental adsorptive capacity (mgS/g)')
-
-ax.legend()
+ax.legend(loc=9)
+plt.savefig('traning_r2.pdf', format='pdf', dpi=1200)
 plt.show()
+os.getcwd()
 
 
-# In[116]:
-
-
-plt.savefig('lm_r2.pdf', format='pdf', dpi=1200)
-
-
-# In[110]:
-
-
-fig.savefig('lm_r2.svg', format='svg', dpi=1200)
-
-
-# In[33]:
-
-
-pd.DataFrame(y_pred).to_csv('y_pred.csv',  index = False)
-pd.DataFrame(y_test).to_csv('y_test.csv', index = False)
-
-
-# In[35]:
+# In[40]:
 
 
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
@@ -310,22 +306,10 @@ print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
 
-# In[117]:
+# In[42]:
 
 
-errors = abs(y_pred - y_test)
-
-
-# In[118]:
-
-
-errors
-
-
-# In[37]:
-
-
-mape = 100 * (errors / y_test)
+mape = 100 * (data_table['errors']/data_table['y_pred'])
 # Calculate and display accuracy
 accuracy = 100 - np.mean(mape)
 print('Accuracy:', round(accuracy, 2), '%.')
@@ -357,6 +341,12 @@ get_ipython().run_line_magic('pinfo', 'pyplot.bar')
 # In[ ]:
 
 
+
+
+
+# In[ ]:
+
+
 X = zeolite_final.drop(["Capacity"], axis = 1)
 X.iloc[:,19]
 
@@ -375,7 +365,7 @@ feat_importances.nlargest(20).plot(kind='barh')
 get_ipython().run_line_magic('pinfo', 'feat_importances.nlargest')
 
 
-# In[96]:
+# In[ ]:
 
 
 get_ipython().run_line_magic('pinfo', 'sns.regplot')
